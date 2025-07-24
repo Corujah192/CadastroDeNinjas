8b23f86dbfc675bcd529512f5d51d5e336bb4272
Corujah192 <corujah192@gmail.com>
2025-07-24 19:00:14 -0300

    Criando a controller do inicio do treino

--- src/main/java/com/treino/cadastroDeNinjas/controllers/CadastroController.java
+++ src/main/java/com/treino/cadastroDeNinjas/controllers/CadastroController.java
@@ -1,4 +1,15 @@
 package com.treino.cadastroDeNinjas.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class CadastroController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas () {
+        return "Essa é minha primeira mensagem nessa rota";}
+
 }
