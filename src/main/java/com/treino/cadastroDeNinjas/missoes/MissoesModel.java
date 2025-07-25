118f30d7415610b8c954f877c2b5e3afa533928f
Corujah192 <corujah192@gmail.com>
2025-07-25 19:32:34 -0300

    Adicionando a dependencia do lomkok e ajustando as os models missoes e ninjas.

--- src/main/java/com/treino/cadastroDeNinjas/missoes/MissoesModel.java
+++ src/main/java/com/treino/cadastroDeNinjas/missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package com.treino.cadastroDeNinjas.missoes;
 
 import com.treino.cadastroDeNinjas.ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
@@ -21,69 +27,4 @@ public class MissoesModel {
 
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjasMissoes;
-
-    public MissoesModel() {}
-
-    public MissoesModel(String nome, String rank, String descricao, int qtdPessoas) {
-        this.nome = nome;
-        this.rank = rank;
-        this.descricao = descricao;
-        this.qtdPessoas = qtdPessoas;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public int getQtdPessoas() {
-        return qtdPessoas;
-    }
-
-    public void setQtdPessoas(int qtdPessoas) {
-        this.qtdPessoas = qtdPessoas;
-    }
-
-    public boolean isStatusMissao() {
-        return statusMissao;
-    }
-
-    public void setStatusMissao(boolean statusMissao) {
-        this.statusMissao = statusMissao;
-    }
-
-    public List<NinjaModel> getNinjasMissoes() {
-        return ninjasMissoes;
-    }
-
-    public void setNinjasMissoes(List<NinjaModel> ninjasMissoes) {
-        this.ninjasMissoes = ninjasMissoes;
-    }
 }
--- src/main/java/com/treino/cadastroDeNinjas/ninjas/NinjaModel.java
+++ src/main/java/com/treino/cadastroDeNinjas/ninjas/NinjaModel.java
@@ -3,11 +3,15 @@ package com.treino.cadastroDeNinjas.ninjas;
 import com.treino.cadastroDeNinjas.missoes.MissoesModel;
 import com.treino.cadastroDeNinjas.tecnicas.TecnicasModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -27,67 +31,4 @@ public class NinjaModel {
     private List<MissoesModel> missoesConcluidas;
     private List<TecnicasModel> tecnicas;
 
-    public NinjaModel () {}
-
-    public NinjaModel(String rank, String cla, String nome) {
-        this.cla = cla;
-        this.rank = rank;
-        this.nome = nome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public String getCla() {
-        return cla;
-    }
-
-    public void setCla(String cla) {
-        this.cla = cla;
-    }
-
-    public MissoesModel getMissoes() {
-        return missoes;
-    }
-
-    public void setMissoes(MissoesModel missoes) {
-        this.missoes = missoes;
-    }
-
-    public List<MissoesModel> getMissoesConcluidas() {
-        return missoesConcluidas;
-    }
-
-    public void setMissoesConcluidas(List<MissoesModel> missoesConcluidas) {
-        this.missoesConcluidas = missoesConcluidas;
-    }
-
-    public List<TecnicasModel> getTecnicas() {
-        return tecnicas;
-    }
-
-    public void setTecnicas(List<TecnicasModel> tecnicas) {
-        this.tecnicas = tecnicas;
-    }
 }
